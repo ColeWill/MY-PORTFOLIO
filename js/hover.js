4a7d57eda9c9ebecc3337be3234846831d50a95a
CoolCode2 <wcole2222@gmail.com>
2017-06-11 00:15:17 -0600

    lots of stuff

--- js/hover.js
+++ js/hover.js
@@ -1,22 +1,35 @@
    $(document).ready(function(){
 
    
+// var pink = 'rgba(250,177,211,1)';
+// var yellow = 'rgba(255,255,82,1)';
+// var blue = 'rgba(200,230,250,1)';
+// var green = 'rgba(80,190,171,1)';
+// var sand = 'rgba(210,180,145,1)';
+// var green2 = 'rgba(130,200,160,1)';
 
+var colorArray = ['green2','yellow','blue','green','pink','sand'];
+var rando = colorArray[(Math.floor(Math.random()* colorArray.length))];
+var colorDiv = '<div class="fadeIn'+rando+'";></div>';
 
         $('.portfolio-item').on('mouseenter', function(event){
             
+                            
 
             var eventObject = event.currentTarget;
             var hiddenDiv = eventObject.getElementsByTagName('div');
             var aTag = $(event.currentTarget).find('a');
             var imgTag = $(aTag).find('img');
+            
                 
+            // console.log(colorDiv);
 
-            console.log(event.currentTarget);
+            // console.log(event.currentTarget);
+            $(hiddenDiv).append(colorDiv);
             $(hiddenDiv).css('visibility','visible');
             $(hiddenDiv).addClass('textChange');
             $(imgTag).addClass('fadeIn');
-            console.log(hiddenDiv);
+            // $(hiddenDiv).append(colorDiv);
         })
             .on('mouseleave', function(event){
                 console.log('mouse left');
@@ -27,6 +40,7 @@
 
                 $(hiddenDiv).css('visibility','hidden');
                 $(imgTag).removeClass('fadeIn');
+
             });
     });
         
